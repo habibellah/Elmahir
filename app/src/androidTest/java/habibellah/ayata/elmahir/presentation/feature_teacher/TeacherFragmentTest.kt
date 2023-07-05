c52d59ee3c1cdd1d8f1704e2b17b7163ca6514a6
habibellah <habibayata1@gmail.com>
2023-07-05 11:52:21 +0200

    Start Testing Teacher Fragment

--- app/src/androidTest/java/habibellah/ayata/elmahir/presentation/feature_teacher/TeacherFragmentTest.kt
+++ app/src/androidTest/java/habibellah/ayata/elmahir/presentation/feature_teacher/TeacherFragmentTest.kt
@@ -1,18 +1,29 @@
 package habibellah.ayata.elmahir.presentation.feature_teacher
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.PerformException
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import habibellah.ayata.elmahir.R
 import habibellah.ayata.elmahir.launchFragmentInHiltContainer
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.instanceOf
+import org.junit.Assert
 import org.junit.Before
-import habibellah.ayata.elmahir.R
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
@@ -34,8 +45,26 @@ class TeacherFragmentTest {
 
    @Test
    fun testLaunchFragmentHiltContainer(){
-      launchFragmentInHiltContainer<TeacherFragment>{
+      launchFragmentInHiltContainer<TeacherFragment>()
+      onView(withId(R.id.add_teacher_fab))
+         .check(matches(isDisplayed()))
+   }
 
+   @Test
+   fun testEmptyRecyclerViewWhenReturnEmptyTeacherList(){
+      var items = 0
+      launchFragmentInHiltContainer<TeacherFragment>(){
+         val recycler = this.view!!.findViewById<RecyclerView>(R.id.teachers_recycler)
+         items = recycler.adapter!!.itemCount
       }
+      Assert.assertEquals(0,items)
+   }
+
+   @Test
+   fun testItemDoesNotExistInTeacherList(){
+      launchFragmentInHiltContainer<TeacherFragment>()
+      onView(withId(R.id.teachers_recycler))
+         .check(matches(instanceOf(RecyclerView::class.java)))
+
    }
 }
