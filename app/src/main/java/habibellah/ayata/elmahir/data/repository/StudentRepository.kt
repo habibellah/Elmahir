9ca7bd721ec5c8eaa9015d034e5e336d61a0a02e
habibellah <habibayata1@gmail.com>
2023-07-07 16:39:09 +0200

    Create StudentRepository

---
+++ app/src/main/java/habibellah/ayata/elmahir/data/repository/StudentRepository.kt
@@ -0,0 +1,16 @@
+package habibellah.ayata.elmahir.data.repository
+
+import androidx.lifecycle.LiveData
+import habibellah.ayata.elmahir.data.roomDb.entity.Absent
+import habibellah.ayata.elmahir.data.roomDb.entity.Student
+
+interface StudentRepository {
+
+   suspend fun addStudent(student : Student)
+
+   suspend fun getStudentBy(groupName : String) : List<Student>
+
+   suspend fun addAbsent(absent : Absent)
+
+   fun getAbsentsBy(studentId : Int) : LiveData<List<Absent>>
+}
---
+++ app/src/main/java/habibellah/ayata/elmahir/data/repository/StudentRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package habibellah.ayata.elmahir.data.repository
+
+import habibellah.ayata.elmahir.data.roomDb.AbsentDao
+import habibellah.ayata.elmahir.data.roomDb.StudentDao
+import habibellah.ayata.elmahir.data.roomDb.entity.Absent
+import habibellah.ayata.elmahir.data.roomDb.entity.Student
+
+class StudentRepositoryImpl (
+   private val studentDao : StudentDao,
+   private val absentDao : AbsentDao
+        ): StudentRepository {
+   override suspend fun addStudent(student : Student) = studentDao.addStudent(student)
+
+   override suspend fun getStudentBy(groupName : String) = studentDao.getStudentsBy(groupName)
+
+   override suspend fun addAbsent(absent : Absent) = absentDao.addAbsent(absent)
+
+   override fun getAbsentsBy(studentId : Int) = absentDao.getAbsentsBy(studentId)
+}
